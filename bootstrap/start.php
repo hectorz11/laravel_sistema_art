1ee3edcc2f080d3b0b3827913973fdf0c01ee002
hectorz11 <hector.rz11@gmail.com>
2015-10-06 11:10:33 -0500

    hectorz11: ...

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -23,12 +23,18 @@ $app = new Illuminate\Foundation\Application;
 | given environment, then we will automatically detect it for you.
 |
 */
-
+/*
 $env = $app->detectEnvironment(array(
 
 	'local' => array('homestead'),
 
-));
+));*/
+
+$env = $app->detectEnvironment(function()
+{
+	return getenv('APP_ENV') ?: 'local';
+});
+
 
 /*
 |--------------------------------------------------------------------------
